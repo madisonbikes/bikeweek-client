296ebe71e4d084f8e423692d3392ab789cef0212
Ben Sandee <tbsandee@orangebikelabs.com>
2022-05-13 09:48:47 -0500

    ui cleanups

--- src/components/forms/EventDetail.tsx
+++ src/components/forms/EventDetail.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Divider, IconButton, Stack } from "@mui/material";
 import { useEffect } from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 import { useMutation, useQuery, useQueryClient } from "react-query";
@@ -7,6 +7,7 @@ import { getEvent, updateEvent } from "../../api/events";
 import { FormTextField } from "../input/FormTextField";
 import { BikeWeekEvent, useAuth } from "../../common";
 import { Box } from "@mui/system";
+import { Delete } from "@mui/icons-material";
 
 type FormData = BikeWeekEvent;
 
@@ -91,98 +92,101 @@ export const EventDetail = () => {
   console.log(`Loaded ${JSON.stringify(data, null, "  ")}`);
 
   return (
-    <div>
-      <form>
-        <Stack>
-          <FormTextField
-            name="name"
-            fullWidth
-            margin="normal"
-            label="Event Name"
-            required
-            control={control}
-          />
-          <FormTextField
-            name="eventUrl"
-            fullWidth
-            margin="normal"
-            label="Event URL"
-            control={control}
-          />
-          <FormTextField
-            name="description"
-            fullWidth
-            multiline
-            required
-            minRows={2}
-            maxRows={10}
-            margin="normal"
-            label="Description"
-            control={control}
-          />
-          <FormTextField
-            name="eventGraphicUrl"
-            fullWidth
-            multiline
-            margin="normal"
-            label="Event Graphic URL"
-            control={control}
-          />
-          {sponsorFields.map((item, index) => (
-            <>
-              <Box display="flex" alignItems="center">
-                <FormTextField
-                  name={`sponsors.${index}.name`}
-                  margin="normal"
-                  label="Sponsor Name"
-                  control={control}
-                />
-                <FormTextField
-                  sx={{ marginLeft: 1, flex: 1 }}
-                  name={`sponsors.${index}.url`}
-                  margin="normal"
-                  label="Sponsor URL"
-                  control={control}
-                />
-                <Button type="button" onClick={() => sponsorFieldRemove(index)}>
-                  Delete
-                </Button>
-              </Box>
-            </>
-          ))}
+    <form>
+      <Stack>
+        <FormTextField
+          name="name"
+          fullWidth
+          margin="normal"
+          label="Event Name"
+          required
+          control={control}
+        />
+        <FormTextField
+          name="eventUrl"
+          fullWidth
+          margin="normal"
+          label="Event URL"
+          control={control}
+        />
+        <FormTextField
+          name="description"
+          fullWidth
+          multiline
+          required
+          minRows={2}
+          maxRows={10}
+          margin="normal"
+          label="Description"
+          control={control}
+        />
+        <FormTextField
+          name="eventGraphicUrl"
+          fullWidth
+          multiline
+          margin="normal"
+          label="Event Graphic URL"
+          control={control}
+        />
+        {sponsorFields.map((_item, index) => (
+          <>
+            <Box display="flex" alignItems="center">
+              <FormTextField
+                sx={{ flex: 1 }}
+                name={`sponsors.${index}.name`}
+                margin="normal"
+                label="Sponsor Name"
+                control={control}
+              />
+              <FormTextField
+                sx={{ marginLeft: 1, flex: 3 }}
+                name={`sponsors.${index}.url`}
+                margin="normal"
+                label="Sponsor URL"
+                control={control}
+              />
+              <IconButton
+                aria-label="delete"
+                sx={{ marginLeft: 0.5 }}
+                onClick={() => sponsorFieldRemove(index)}
+              >
+                <Delete />
+              </IconButton>
+            </Box>
+          </>
+        ))}
+        <Button
+          type="button"
+          onClick={() => sponsorFieldAppend({ name: "", url: "" })}
+        >
+          Add Sponsor
+        </Button>
+        <Divider />
+        <Box sx={{ marginTop: 1 }}>
           <Button
-            type="button"
-            onClick={() => sponsorFieldAppend({ name: "", url: "" })}
+            disabled={isSubmitting}
+            onClick={handleSubmit((formData) => {
+              eventMutation.mutate(formData);
+              form.reset(formData);
+            })}
+            variant="contained"
           >
-            Add Sponsor
+            Save
           </Button>
 
-          <div>
-            <Button
-              disabled={isSubmitting}
-              onClick={handleSubmit((formData) => {
-                eventMutation.mutate(formData);
-                form.reset(formData);
-              })}
-              variant="contained"
-            >
-              Save
-            </Button>
-
-            <Button
-              style={{ marginLeft: "1em" }}
-              disabled={isSubmitting}
-              onClick={() => {
-                navigate("/events");
-              }}
-              variant="contained"
-            >
-              Close
-            </Button>
-          </div>
-        </Stack>
-      </form>
-    </div>
+          <Button
+            style={{ marginLeft: "1em" }}
+            disabled={isSubmitting}
+            onClick={() => {
+              navigate("/events");
+            }}
+            variant="contained"
+          >
+            Close
+          </Button>
+        </Box>
+      </Stack>
+    </form>
   );
 };
 
