2f771148f69d9ddbd3e1b56d27417381bf59c645
Ben Sandee <tbsandee@orangebikelabs.com>
2022-05-18 13:47:39 -0500

    add link to bike week page

--- src/components/forms/eventdetail/Location.tsx
+++ src/components/forms/eventdetail/Location.tsx
@@ -16,6 +16,8 @@ const Location = () => {
         label="Name"
         control={control}
       />
+      {/*
+      Unused in 2022
       <FormTextField
         name="location.sched_venue"
         fullWidth
@@ -23,14 +25,20 @@ const Location = () => {
         label="Sched venue name override"
         control={control}
       />
-      <FormTextField
-        name="location.sched_address"
-        fullWidth
-        margin="normal"
-        label="Sched venue address override"
-        control={control}
-      />
-
+      */}
+      <div style={{ marginTop: "1rem" }}>
+        Sched requires an actual address, put the closest address you can find
+        including city/zip here.
+        <FormTextField
+          name="location.sched_address"
+          fullWidth
+          margin="normal"
+          label="Sched venue address override"
+          control={control}
+        />
+      </div>
+      {/*
+      Unused in 2022
       <FormTextField
         name="location.maps_description"
         fullWidth
@@ -38,20 +46,30 @@ const Location = () => {
         label="Short location description override for inclusion in event"
         control={control}
       />
-      <FormTextField
-        name="location.maps_query"
-        fullWidth
-        margin="normal"
-        label="Google Maps query name"
-        control={control}
-      />
-      <FormTextField
-        name="location.maps_placeid"
-        fullWidth
-        margin="normal"
-        label="Google Maps placeid"
-        control={control}
-      />
+      */}
+      <div style={{ marginTop: "1rem" }}>
+        Optional: If the location name above is not enough to ID the location
+        for Google Maps, you can override it here. Include GPS coordinates or a
+        detailed maps query string.
+        <FormTextField
+          name="location.maps_query"
+          fullWidth
+          margin="normal"
+          label="Google Maps query name"
+          control={control}
+        />
+      </div>
+      <div style={{ marginTop: "1rem" }}>
+        Optional: Mostly unnecessary but you can include this to improve Google
+        Maps behavior in certain circumstances where the maps query is not
+        <FormTextField
+          name="location.maps_placeid"
+          fullWidth
+          margin="normal"
+          label="Google Maps placeid"
+          control={control}
+        />
+      </div>
       <FormTextField
         name="location.detailed_location_description"
         fullWidth
