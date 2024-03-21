b3a990486fa610db1e70742b373e570d49166a62
Ben Sandee <ben@orangebikelabs.com>
2024-03-21 12:24:24 -0500

    fix: update config for 2024

--- src/common/config.tsx
+++ src/common/config.tsx
@@ -1,7 +1,8 @@
-export const FIRST_DAY = "06/03/2023";
-export const LAST_DAY = "06/11/2023";
-export const SCHED_URI = "https://madisonbikeweek2023.sched.com";
-export const GF_FORM_ID = 8;
+// TODO someday pull these from the backend, it already knows this stuff
+export const FIRST_DAY = "06/01/2024";
+export const LAST_DAY = "06/09/2024";
+export const SCHED_URI = "https://madisonbikeweek2024.sched.com";
+export const GF_FORM_ID = 11;
 export const googleLoginEnabled = Boolean(
   process.env.VITE_APP_GOOGLE_CLIENT_ID ?? "",
 );
