4cdd1a4658231de8eb1b98c36c730137f3987fe1
Ben Sandee <ben@orangebikelabs.com>
2024-04-04 11:39:08 -0500

    chore: lint errors

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,9 @@
-import { useAuth } from "../common";
 import { ChangePassword } from "./forms/ChangePassword";
 import { GoogleLink } from "./GoogleLink";
 import { GoogleUnlink } from "./GoogleUnlink";
 import { Logout } from "./Logout";
 
 export const Profile = () => {
-  const auth = useAuth();
   return (
     <div>
       <ChangePassword />
