2972e71b3f2aa289d355896b0197f4a379526a09
Ben Sandee <ben@orangebikelabs.com>
2025-05-02 14:21:48 -0500

    feat: update for 2025

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,10 +1,10 @@
 import { parse } from "date-fns";
 
 // TODO someday pull these from the backend, it already knows this stuff
-export const FIRST_DAY = parse("06/01/2024", "MM/dd/yyyy", new Date());
-export const LAST_DAY = parse("06/09/2024", "MM/dd/yyyy", new Date());
-export const SCHED_URI = "https://madisonbikeweek2024.sched.com";
-export const GF_FORM_ID = 11;
+export const FIRST_DAY = parse("05/31/2025", "MM/dd/yyyy", new Date());
+export const LAST_DAY = parse("06/08/2025", "MM/dd/yyyy", new Date());
+export const SCHED_URI = "https://madisonbikeweek2025.sched.com";
+export const GF_FORM_ID = 13;
 export const googleLoginEnabled = Boolean(
   import.meta.env.VITE_APP_GOOGLE_CLIENT_ID ?? "",
 );
